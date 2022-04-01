a234b9872177c6e54a73de9da1f21cc0e74161eb
VanessaGuzman12 <vanemaguh@gmail.com>
2022-04-01 19:28:48 +0800

    new file

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+import os
+import csv
+file= os.path.join('.','Resources','budget_data.csv')
+
+#with open (file,'r') as csv:
+ #csvreader = csv.read(file, delimiter=',')
+ #header = next(csvreader)
+  
+  # num_months= []
+  
+   #for i in range(len(num_months)):
+   # print("[" + str(i) + "] " + num_months[i])
+print (os.listdir(file))
+print (file)
+
