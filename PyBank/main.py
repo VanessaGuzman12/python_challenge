a9b3befaaf5e5785ed0729b1ae54668a8d2a8e3b
VanessaGuzman12 <vanemaguh@gmail.com>
2022-04-01 23:42:25 +0800

    Add new modifies

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,37 @@
 import os
 import csv
-file= os.path.join('.','Resources','budget_data.csv')
+#path2= os.path.join('Resources','budget_data.csv')
+path2="C:/Users/vanem/Homeworks/HomeworkPython/python_challenge/PyBank/Resources/budget_data.csv"
 
-#with open (file,'r') as csv:
- #csvreader = csv.read(file, delimiter=',')
- #header = next(csvreader)
-  
-  # num_months= []
-  
-   #for i in range(len(num_months)):
-   # print("[" + str(i) + "] " + num_months[i])
-print (os.listdir(file))
-print (file)
+def load_file (file):
+    with open (file,'r') as data:
+        csvreader = csv.reader(data, delimiter=',')
+        header = next(csvreader)
+        #print(f"Header: {header}")
+    return tabla_1
+
+def number_months (bank_record):
+    for row in :
+
+    
+    
+    
+    
+    
+    number_months= int (bank_record)
+    average= int(bank_record)
+    greatest_increase =int (bank_record)
+    greatest_decrease=int (bank_record)
+
+
+
+
+
+
+
+
+    
+
+load_file(path2)
+
 
