adaec685336df3450a1052b1c9d8d720dabc235a
VanessaGuzman12 <vanemaguh@gmail.com>
2022-04-03 13:58:54 +0800

    changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,24 +3,24 @@ import csv
 #path2= os.path.join('Resources','budget_data.csv')
 path2="C:/Users/vanem/Homeworks/HomeworkPython/python_challenge/PyBank/Resources/budget_data.csv"
 
-#def load_file (filepath):
+
 with open (path2,'r') as data:
     csvreader = csv.reader(data, delimiter=',')
     header = next(csvreader)
     print(f"Header: {header}")
-        #return data.read()
+        
     
     records= []
     for row in csvreader:
         records.append(row)
 
 num_months=len(records)
-print("Total Months: ", num_months)
+#print("Total Months: ", num_months)
 
 total_amount= 0
 for i in range(0, num_months):
     total_amount+=int(records[i][1])
-print ("Total: ", total_amount)
+#print ("Total: ", total_amount)
 
 avg_list= []
 for i in range(0,num_months-1):
@@ -28,7 +28,7 @@ for i in range(0,num_months-1):
     avg_list.append(change)
 
 avg_change=sum(avg_list)/len(avg_list)
-print("Average Change: ", avg_change)
+#print("Average Change: ", avg_change)
 
 date_increase= records[0][0]
 greatest_increase= int(records[0][1])
@@ -36,7 +36,7 @@ for i in range(0,num_months):
     if greatest_increase < int(records[i][1]):
         greatest_increase= int(records[i][1])
         date_increase= records[i][0]
-print("Greatest Increase in Profits: ",date_increase, greatest_increase)
+#print("Greatest Increase in Profits: ",date_increase, greatest_increase)
 
 date_decrease= records[0][0]
 greatest_decrease= int(records[0][1])
@@ -44,10 +44,17 @@ for i in range(0,num_months):
     if greatest_decrease > int(records[i][1]):
         greatest_decrease= int(records[i][1])
         date_decrease= records[i][0]
-print("Greatest Decrease in Profits: ",date_decrease, greatest_decrease)
+#print("Greatest Decrease in Profits: ",date_decrease, greatest_decrease)
 
-    
-    
+print ("Financial Analysis")
+print("_______________________")
+print("Total Months: ", num_months)
+print ("Total: ", total_amount)
+print("Average Change: ", avg_change)
+print(f"Greatest Increase in Profits: {date_increase} ${greatest_increase:,.2f}")
+print(f"Greatest Decrease in Profits: {date_decrease} ${greatest_decrease:,.2f}")
+  
+
     
     
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,10 @@
 import os
 import csv
-#path2= os.path.join('Resources','budget_data.csv')
-path2="C:/Users/vanem/Homeworks/HomeworkPython/python_challenge/PyPoll/Resources/election_data.csv"
+path2= os.path.join('Resources','budget_data.csv')
+#path2="C:/Users/vanem/Homeworks/HomeworkPython/python_challenge/PyPoll/Resources/election_data.csv"
 
 #def load_file (filepath):
-with open (path2,'r') as data:
+with open (path2,"r") as data:
     csvreader = csv.reader(data, delimiter=',')
     header = next(csvreader)
     #print(f"Header: {header}")
@@ -15,7 +15,7 @@ with open (path2,'r') as data:
         records.append(row)
 
 num_votes=len(records)
-print("Total Votes: ", num_votes)
+#print("Total Votes: ", num_votes)
 
 candidates=[]
 for i in range(0,num_votes):
@@ -23,7 +23,7 @@ for i in range(0,num_votes):
         candidates==candidates
     else:
         candidates.append(records[i][2])
-print (candidates)
+#print (candidates)
 
 votes_candidate= []
 for i in range(len(candidates)):
@@ -33,10 +33,30 @@ for i in range(num_votes):
     for j in range(len(candidates)):
         if records[i][2]== candidates[j]:
             votes_candidate[j]+=1
+#print (votes_candidate)
 
-print (votes_candidate)
+percentage_votes= []
+for i in range(len(votes_candidate)):
+    percentage_votes.append((votes_candidate[i]/num_votes))
+#print (percentage_votes)
+
+winner= candidates[0]
+max_votes=votes_candidate[0]
+for i in range(len(votes_candidate)):
+    if max_votes < votes_candidate[i]:
+        max_votes= votes_candidate[i]
+        winner=candidates[i]
+#print ("Winner: ", winner)
 
 
+print ("Election results")
+print("_______________________")
+print("Total Votes: ", num_votes)
+print("________________________")
+for i in range(4):
+   print(f"{candidates[i]}: {percentage_votes[i]:.3%} ({votes_candidate[i]:,})")
+print("______________________")
+print ("Winner: ", winner)
+print("_______________________")
 
-
-
+ 
